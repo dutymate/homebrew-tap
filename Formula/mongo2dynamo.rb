c97cf242ba8053381e1d1eaf019b0cee7b36c6e1
Jongwoo Han <jongwooo.han@gmail.com>
2025-07-03 06:21:26 +0000

    release: Update mongo2dynamo to v1.3.0

--- Formula/mongo2dynamo.rb
+++ Formula/mongo2dynamo.rb
@@ -1,28 +1,28 @@
 class Mongo2dynamo < Formula
   desc "CLI tool to migrate data from MongoDB to DynamoDB"
   homepage "https://github.com/dutymate/mongo2dynamo"
-  version "1.2.0"
+  version "1.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel? # darwin_amd64
-      url "https://github.com/dutymate/mongo2dynamo/releases/download/v1.2.0/mongo2dynamo_Darwin_x86_64.tar.gz"
-      sha256 "67860f957c29eb3bff7e8885068f578ae4ccc976d7a121aa6c323a3921cc51ba"
+      url "https://github.com/dutymate/mongo2dynamo/releases/download/v1.3.0/mongo2dynamo_Darwin_x86_64.tar.gz"
+      sha256 "86ef80e0207f7c9bf367998b72ef76e23f6375cda13d03d717650a66e7163dc4"
     end
     if Hardware::CPU.arm? # darwin_arm64
-      url "https://github.com/dutymate/mongo2dynamo/releases/download/v1.2.0/mongo2dynamo_Darwin_arm64.tar.gz"
-      sha256 "ff9df6d71f956678c2575c063fc03010f89c39eb0a6e73440a59e0a3b2f012e0"
+      url "https://github.com/dutymate/mongo2dynamo/releases/download/v1.3.0/mongo2dynamo_Darwin_arm64.tar.gz"
+      sha256 "f6e1edaf832adb6eeca1563c3a7a47f7bd4da0ca1fbb32b267fa122033a07078"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? # linux_amd64
-      url "https://github.com/dutymate/mongo2dynamo/releases/download/v1.2.0/mongo2dynamo_Linux_x86_64.tar.gz"
-      sha256 "0582258fbbc83bbe3a3e82a2a5c818003a332a4ba10be7ffbbc6c6409d8d7a68"
+      url "https://github.com/dutymate/mongo2dynamo/releases/download/v1.3.0/mongo2dynamo_Linux_x86_64.tar.gz"
+      sha256 "2c763328c3cba818a3db3c887b1b90a4844931151a63f72cdeab5f24deabfb0f"
     end
     if Hardware::CPU.arm? # linux_arm64
-      url "https://github.com/dutymate/mongo2dynamo/releases/download/v1.2.0/mongo2dynamo_Linux_arm64.tar.gz"
-      sha256 "6fe42724696735f834e231effaa3ae9503bbf2e3cf194c9329e74ebf22d38c4e"
+      url "https://github.com/dutymate/mongo2dynamo/releases/download/v1.3.0/mongo2dynamo_Linux_arm64.tar.gz"
+      sha256 "ea93ab15ddb03b921be40328c1e703a362a16e0ce96c87a813ff9882b016ff82"
     end
   end
 
